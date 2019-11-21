7305229918ff72da5f1721dcf2b1441e80f6e2f4
simochee <simochee@gmail.com>
2019-11-21 22:58:33 +0900

    ⚡️ Added export package information

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 // @ts-ignore
 import TypedReactive from '@lollipop-onl/vue-typed-reactive';
 
-export default () => {
+export default function TypedReactiveModule () {
   Vue.use(TypedReactive);
 };
+
+module.exports.meta = require('../package.json');
