3f6b8b57dadeb1ba8cfc75de003d2eb1567d299e
simochee <simochee@gmail.com>
2019-11-22 15:38:44 +0900

    ⚡️ Allow nullable value

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { WritableKeys, OptionalKeys } from './utils';
 declare module 'vue/types/vue' {
   interface Vue {
     /** Type-safe version of Vue.set */
-    $typedSet<T extends Object, K extends WritableKeys<T>>(object: T, key: K, value: NonNullable<T[K]>): T[K];
+    $typedSet<T extends Object, K extends WritableKeys<T>>(object: T, key: K, value: T[K]): T[K];
     /** Type-safe version of Vue.delete */
     $typedDelete<T extends Object, K extends OptionalKeys<T>>(object: T, key: K): void;
   }
   
   interface VueConstructor {
     /** Type-safe version of vm.$set */
-    typedSet<T extends Object, K extends WritableKeys<T>>(object: T, key: K, value: NonNullable<T[K]>): T[K];
+    typedSet<T extends Object, K extends WritableKeys<T>>(object: T, key: K, value: T[K]): T[K];
     /** Type-safe version of vm.$delete */
     typedDelete<T extends Object, K extends OptionalKeys<T>>(object: T, key: K): void;
   }
