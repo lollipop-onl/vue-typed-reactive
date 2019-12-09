1aa6fc3cc564e7e88838dcb1f67af949758eb2e6
lollipop-onl <lollipop@simoochee.net>
2019-12-09 23:58:17 +0900

    🔨 unification notation

--- src/utils.ts
+++ src/utils.ts
@@ -14,19 +14,19 @@ export type IsEquals<X, Y> =
   (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
 
 /** is optional property */
-export type IsOptional<T extends Object, P extends keyof T> = IsEquals<Pick<T, P>, { [K in P]?: T[P] }>;
+export type IsOptional<T extends Object, P extends keyof T> = IsEquals<{ [K in P]: T[P] }, { [K in P]?: T[P] }>;
 
 /** is readonly property */
 export type IsNotReadonly<T extends Object, P extends keyof T> = IsEquals<{ [K in P]: T[P] }, { -readonly [K in P]: T[P]}>;
 
 /** Pick writable property keys */
-export type WritableKeys<T extends Object> = 
+export type WritableKeys<T extends Object> =
   T extends any[] ? number : Values<{
     [P in keyof T]-?: IsNotReadonly<T, P> extends true ? P : never;
   }>;
 
 /** Pick optional property keys */
-export type OptionalKeys<T extends Object> = 
+export type OptionalKeys<T extends Object> =
   T extends any[] ? number : Values<{
     [P in keyof T]-?: And<IsOptional<T, P>, IsNotReadonly<T, P>> extends true ? P : never;
   }>;
